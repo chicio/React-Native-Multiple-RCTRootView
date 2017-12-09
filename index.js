9c1e9caf3d61cf4d3117be43bf0c519216fd5f9e
chicio <fabrizio.duroni@lastminute.com>
2017-12-09 02:10:16 +0100

    Fixed stylesheet for markdown :bug:

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import {View, AppRegistry} from "react-native";
+import {View, AppRegistry, StyleSheet} from "react-native";
 
 class BlueScreen extends React.Component {
     render() {
         return (
-            <View style={{backgroundColor: "#0000FF", width: "100%", height: "100%"}} />
+            <View style={styles.blue} />
         );
     }
 }
@@ -12,10 +12,23 @@ class BlueScreen extends React.Component {
 class RedScreen extends React.Component {
     render() {
         return (
-            <View style={{backgroundColor: "#FF0000", width: "100%", height: "100%"}} />
+            <View style={styles.red} />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    blue: {
+        backgroundColor: "#0000FF",
+        width: "100%",
+        height: "100%"
+    },
+    red: {
+        backgroundColor: "#FF0000",
+        width: "100%",
+        height: "100%"
+    }
+});
 
 AppRegistry.registerComponent('BlueScreen', () => BlueScreen);
 AppRegistry.registerComponent('RedScreen', () => RedScreen);
